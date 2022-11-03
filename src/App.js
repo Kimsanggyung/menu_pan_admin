b5cb7d15e96e038215df1a48ddb5b4dac6e1d580
Kim sanggyung <ksang0906@gmail.com>
2022-11-03 16:43:32 +0900

    기본적인 화면구현 테스트 중

--- src/App.js
+++ src/App.js
@@ -2,22 +2,43 @@ import './App.css';
 import './components/Login'
 import Login from './components/Login';
 import Main from './components/Main';
+import Logout from './components/Logout';
 import AddMenu from './components/AddMenu';
+import { useState } from 'react';
 
 function App() {
+
+  const [login, setLogin] = useState(false)
+  const [stateData, setStateData] = useState()
+  let view = null;
+
+  if(login === false){
+    view = <Login setLogin={setLogin}></Login>
+  }
+  if(login === true){
+    view =
+      <>
+        <Logout setLogin={setLogin}></Logout>
+        <Main setStateData={setStateData}></Main>
+      </> 
+      
+  }
+  if(login === true && stateData === 'addMenu'){
+    view =
+      <>
+        <Logout setLogin={setLogin}></Logout>
+        <Main></Main>
+        <AddMenu></AddMenu>
+      </> 
+      
+  }
+
   return (
     <>
       <div className="App">
-        <Login></Login>
+        {view}
       </div>
-      {/* <div className='main'>
-        <Main></Main>
-      </div>
-      <div>
-        <AddMenu></AddMenu>
-      </div> */}
     </>
-    
   );
 }
 
