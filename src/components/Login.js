aa76cd224fb9e3cc3db22398835d61aa2129e4f8
Kim sanggyung <ksang0906@gmail.com>
2022-11-03 16:44:14 +0900

    로그인API 데스트 겸 axios테스트

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import '../css/Login.css'
+import axios from 'axios'
 
-function Login(){
+function Login({setLogin}){
   
   const [inputID, setInputID] = useState()
   const [inputPWD, setInputPWD] = useState()
@@ -12,12 +13,27 @@ function Login(){
   const inputPwdChange = event => { // 비밀번호 입력창에 입력을 하는 등 이벤트가 발생하면 setInputPWD
     setInputPWD(event.target.value);
   };
+  
   const login = () => {
-    console.log("ID", inputID, "PWD", inputPWD)
+    axios
+      .post("http://127.0.0.1:8000/login/", {
+        id: inputID,
+        password: inputPWD
+      })
+      .then(function (response){
+        console.log(response.status);
+      })
+      .catch(function (error){
+        console.log(error)
+        console.log("틀림"); // 콘솔로그에 에러보여주기
+      });
   }
 
   return( 
     <div>
+      <div>
+        <h1 className="title">관리자 사이트</h1>
+      </div>
       <div>
         <form className="form">
             <h3>로그인</h3>
@@ -31,8 +47,11 @@ function Login(){
               <label className="save"><input type="checkbox"></input>아이디저장</label>
               <label className="auto"><input type="checkbox"></input>자동로그인</label>
             </div>
-            <div className="loginbutton">
-              <button type="button" onClick={login}>로그인</button>
+            <div>
+              <button className="loginbutton" type="button" onClick={login}>로그인</button>
+            </div>
+            <div className="signup">
+              <button className="singupbutton" type="button" onClick={login}>회원가입</button>
             </div>
         </form>
       </div>
