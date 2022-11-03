ef1a1737c625a44b816fbd5cf2f406d7ae1d7c31
Kim sanggyung <ksang0906@gmail.com>
2022-11-03 16:45:23 +0900

    사용하지 않는 부분 제거

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,22 @@ import { useEffect, useState } from 'react';
 import Itmes from './Items';
 import Buttons from './Buttons';
 
-function Main() {
+function Main({setStateData}) {
 
   const [view, setView] = useState();
   useEffect(()=>{
     console.log(view)
   },[view])
 
+  const viewAdd = () => {
+    setStateData('addMenu')
+  }
+
   return (
     <div>
       <h1 className="App">Menu Pan</h1>
       <Buttons setView={setView}></Buttons>
+      <button onClick={viewAdd}>메뉴추가</button>
     </div>
   );
 }
