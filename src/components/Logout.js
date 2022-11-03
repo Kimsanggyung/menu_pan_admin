576f2b9c21bcf2e2ac550bedbdb958a735142d99
Kim sanggyung <ksang0906@gmail.com>
2022-11-03 16:44:41 +0900

    로그아웃 컴포넌트 생성

---
+++ src/components/Logout.js
@@ -0,0 +1,12 @@
+function Logout({setLogin}){
+  const logout = () => {
+    setLogin(false)
+  }
+
+  return(
+    <button className="font-Do w-20 h-8 ml-4 rounded border-2 text-xl border-gray-500" onClick={logout}>로그아웃</button>
+  );
+
+}
+
+export default Logout;
