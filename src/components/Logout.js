3f146f7a3805d91bd72d9b32699de9dd9a25d2ea
Kim sanggyung <ksang0906@gmail.com>
2022-11-16 15:29:03 +0900

    로그아웃 기능구현 식별 문제가 있다

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,26 @@
-function Logout({setLogin}){
+import axios from "axios";
+
+function Logout({token, setStateData, setLogin}){
+  console.log(token)
   const logout = () => {
     setLogin(false)
+    setStateData('login')
+    localStorage.clear()
+    sessionStorage.clear()
+    axios
+      .delete(`http://127.0.0.1:8000/logout/`,{
+        headers: {
+          Authorization: `Token ${token}`
+        }
+      })
+      .then(()=>{
+        localStorage.clear()
+        sessionStorage.clear()
+        console.log("success")
+      })
+      .catch(function(error){
+        console.log(error);
+      });
   }
 
   return(
