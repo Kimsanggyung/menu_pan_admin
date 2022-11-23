ae8db71d4667ce7403438e776996004e6dcbb475
Kim sanggyung <ksang0906@gmail.com>
2022-11-23 16:43:03 +0900

    수정 버튼 추가

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,16 +2,19 @@ import axios from "axios";
 import '../css/Items.css'
 import { useEffect, useState } from "react";
 
-function Itmes({menuList, setList}){
+function Itmes({menuList, setList, setTargetID, setStateData}){
 
   const [Items, setItems] = useState();
 
-  
+  const Detail = (id) => {
+    setStateData('detail')
+    setTargetID(id)
+  }
 
   useEffect(()=>{
     axios.get("http://127.0.0.1:8000/menu/")
     .then(function(response){
-      console.log(response)
+      console.log(response.status)
       setList(response.data)
     })
     .catch(function(response){
@@ -24,7 +27,7 @@ function Itmes({menuList, setList}){
       setItems(
         menuList.map((menu, index)=>{
           return(
-            <div key={index} className="itemtop" >
+            <div key={index} className="itemtop" onClick={()=>{Detail(menu.id)}}>
               <div>
                 <img src={menu.image} alt={menu.name + "사진"} className="itemimg"></img>
               </div>  
@@ -35,12 +38,14 @@ function Itmes({menuList, setList}){
 
               <div className='price'>
                 <span>상품가격: </span>
+                <div>{menu.price}</div>
               </div>
             </div>
           )
         })
       )
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[menuList])
 
   
