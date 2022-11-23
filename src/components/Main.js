758dfd8dfe6df2cd51df39429c183072d8823d1e
Kim sanggyung <ksang0906@gmail.com>
2022-11-23 12:36:11 +0900

    전체 메뉴 보기와 카테 고리 별 보기 구분

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,20 @@
 import { useEffect, useState } from 'react';
 import Itmes from './Items';
+import Kategorie from './Kategorie';
 import Buttons from './Buttons';
 
-function Main({setStateData}) {
+function Main({menuList, kategorie, setKategorie, setList, setStateData}) {
 
-  const [view, setView] = useState();
+  const [Items, setItems] = useState()
+
   useEffect(()=>{
-    console.log(view)
-  },[view])
+    if(kategorie === 'all'){
+      setItems(<Itmes menuList={menuList} setList={setList}/>)
+    }
+    if(kategorie !== 'all'){
+      setItems(<Kategorie menuList={menuList} kategorie={kategorie} ></Kategorie>)
+    }
+  },[menuList, kategorie, setList])
 
   const viewAdd = () => {
     setStateData('addMenu')
@@ -17,11 +24,13 @@ function Main({setStateData}) {
     <>
       <div>
         <h1 className="App">Menu Pan</h1>
-        <Buttons setView={setView}></Buttons>
+        <Buttons setKategorie={setKategorie}></Buttons>
+      </div>
+      <div>
         <button onClick={viewAdd}>메뉴추가</button>
       </div>
       <div>
-        <Itmes/>
+        {Items}
       </div>
     </>
   );
