3e702cdcfcfc0dcc4ebaeef090bca5316f165f3e
Kim sanggyung <ksang0906@gmail.com>
2022-11-24 11:20:50 +0900

    아이템 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Itmes from './Items';
 import Kategorie from './Kategorie';
 import Buttons from './Buttons';
+import '../css/main.css'
 
 function Main({targetID, setTargetID, menuList, kategorie, setKategorie, setList, setStateData}) {
 
@@ -9,10 +10,30 @@ function Main({targetID, setTargetID, menuList, kategorie, setKategorie, setList
 
   useEffect(()=>{
     if(kategorie === 'all'){
-      setItems(<Itmes setStateData={setStateData} menuList={menuList} setList={setList} setTargetID={setTargetID}/>)
+      setItems(
+        <>
+          <Itmes setStateData={setStateData} menuList={menuList} setList={setList} setTargetID={setTargetID}/>
+          <div className='addtop'>
+            <button className='addsize' onClick={viewAdd}>메뉴추가
+            <div className='plus'>+</div>
+            </button>
+    
+          </div>
+        </>
+      
+      )
     }
     if(kategorie !== 'all'){
-      setItems(<Kategorie setStateData={setStateData} menuList={menuList} kategorie={kategorie} setTargetID={setTargetID}></Kategorie>)
+      setItems(
+        <>
+          <Kategorie setStateData={setStateData} menuList={menuList} setList={setList} setTargetID={setTargetID}/>
+          <div className='addtop'>
+            <button className='addsize' onClick={viewAdd}>메뉴추가
+              <div className='plus'>+</div>
+            </button>
+          </div>
+      </>
+      )
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[menuList, kategorie])
@@ -27,9 +48,7 @@ function Main({targetID, setTargetID, menuList, kategorie, setKategorie, setList
         <h1 className="App">Menu Pan</h1>
         <Buttons setKategorie={setKategorie}></Buttons>
       </div>
-      <div>
-        <button onClick={viewAdd}>메뉴추가</button>
-      </div>
+
       <div>
         {Items}
       </div>
